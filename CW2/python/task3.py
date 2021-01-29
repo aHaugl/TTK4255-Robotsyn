6dba3f913003a41e5e5bdeae0cc860fb72579a53
Andreas <andreas.haugland1@gmail.com>
2021-01-29 09:31:41 +0100

    Done task 3 + tested with extra img

--- CW2/python/task3.py
+++ CW2/python/task3.py
@@ -1,9 +1,12 @@
 import matplotlib.pyplot as plt
+import cv2
 import numpy as np
 from common import *
 
 # Note: the sample image is naturally grayscale
-I = rgb_to_gray(im2double(plt.imread('../data/calibration.jpg')))
+# I = rgb_to_gray(im2double(plt.imread('../data/calibration.jpg')))
+# I = rgb_to_gray(im2double(plt.imread('../data/test1.jpg')))
+I = rgb_to_gray(im2double(plt.imread('../data/test3.png')))
 
 ###########################################
 #
@@ -13,7 +16,23 @@ I = rgb_to_gray(im2double(plt.imread('../data/calibration.jpg')))
 sigma_D = 1
 sigma_I = 3
 alpha = 0.06
-response = np.zeros_like(I) # Placeholder
+
+
+I_diff_smooth = cv2.blur(I, (sigma_D,sigma_D))
+I_dx, I_dy, _ = derivative_of_gaussian(I, sigma_D)
+
+I_dxx = I_dx**2
+I_dxyy = I_dy**2
+I_dxy = I_dx*I_dy
+
+I_dxx_smooth = gaussian(I_dxx, sigma_I)
+I_dyy_smooth = gaussian(I_dxyy, sigma_I)
+I_dxy_smooth = gaussian(I_dxy, sigma_I)
+
+det = I_dxx_smooth * I_dyy_smooth - I_dxy_smooth**2
+trace = I_dxx_smooth + I_dyy_smooth
+
+response = det - (alpha * trace**2)
 
 ###########################################
 #
@@ -23,7 +42,7 @@ response = np.zeros_like(I) # Placeholder
 corners_y = [0] # Placeholder Hente ut vertikale hjørner?
 corners_x = [0] # Placeholder Hente ut horisontale hjørnekomponenter?
 
-
+corners_y, corners_x = extract_local_maxima(response, 0.001)
 
 ###########################################
 #
